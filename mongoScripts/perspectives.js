4f6a1873f6469b4448a899f1fdb96805d4a61b1c
Jeff Ellis <jeff@ellises.us>
2017-09-27 15:43:32 -0400

    Refactor field names

--- mongoScripts/perspectives.js
+++ mongoScripts/perspectives.js
@@ -10,63 +10,63 @@ collection.deleteMany({sourceSet: sourceSet});
 collection.insert([
     {
         "sourceSet": sourceSet,
-        "area": "Easy to release", 
-        "green": "Releasing is simple, safe, painless & mostly automated.",
-        "red": "Releasing is risky, painful, lots of manual work, and takes forever."
+        "topic": "Easy to release", 
+        "awesome": "Releasing is simple, safe, painless & mostly automated.",
+        "crappy": "Releasing is risky, painful, lots of manual work, and takes forever."
     },
     {
         "sourceSet": sourceSet,
-        "area": "Suitable process", 
-        "green": "Our way of working fits us perfectly",
-        "red": "Our way of working sucks"
+        "topic": "Suitable process", 
+        "awesome": "Our way of working fits us perfectly",
+        "crappy": "Our way of working sucks"
     },
     {
         "sourceSet": sourceSet,
-        "area": "Tech quality (code base health)", 
-        "green": "We’re proud of the quality of our code! It is clean, easy to read, and has great test coverage.",
-        "red": "Our code is a pile of dung, and technical debt is raging out of control"
+        "topic": "Tech quality (code base health)", 
+        "awesome": "We’re proud of the quality of our code! It is clean, easy to read, and has great test coverage.",
+        "crappy": "Our code is a pile of dung, and technical debt is raging out of control"
     },
     {
         "sourceSet": sourceSet,
-        "area": "Value", 
-        "green": "We deliver great stuff! We’re proud of it and our stakeholders are really happy.",
-        "red": "We deliver crap. We feel ashamed to deliver it. Our stakeholders hate us."
+        "topic": "Value", 
+        "awesome": "We deliver great stuff! We’re proud of it and our stakeholders are really happy.",
+        "crappy": "We deliver crap. We feel ashamed to deliver it. Our stakeholders hate us."
     },
     {
         "sourceSet": sourceSet,
-        "area": "Speed", 
-        "green": "We get stuff done really quickly.No waiting, no delays.",
-        "red": "We never seem to get done with anything.We keep getting stuck or interrupted. Stories keep getting stuck on dependencies."
+        "topic": "Speed", 
+        "awesome": "We get stuff done really quickly.No waiting, no delays.",
+        "crappy": "We never seem to get done with anything.We keep getting stuck or interrupted. Stories keep getting stuck on dependencies."
     },
     {
         "sourceSet": sourceSet,
-        "area": "Mission", 
-        "green": "We know exactly why we are here, and we are really excited about it.",
-        "red": "We have no idea why we are here, there is no high level picture or focus. Our so-called mission is completely unclear and uninspiring."
+        "topic": "Mission", 
+        "awesome": "We know exactly why we are here, and we are really excited about it.",
+        "crappy": "We have no idea why we are here, there is no high level picture or focus. Our so-called mission is completely unclear and uninspiring."
     },
     {
         "sourceSet": sourceSet,
-        "area": "Fun", 
-        "green": "We love going to work, and have great fun working together.",
-        "red": "Boooooooring."
+        "topic": "Fun", 
+        "awesome": "We love going to work, and have great fun working together.",
+        "crappy": "Boooooooring."
     },
     {
         "sourceSet": sourceSet,
-        "area": "Learning", 
-        "green": "We’re learning lots of interesting stuff all the time!",
-        "red": "We never have time to learn anything"
+        "topic": "Learning", 
+        "awesome": "We’re learning lots of interesting stuff all the time!",
+        "crappy": "We never have time to learn anything"
     },
     {
         "sourceSet": sourceSet,
-        "area": "Support", 
-        "green": "We always get great support & help when we ask for it!",
-        "red": "We keep getting stuck because we can’t get the support & help that we ask for."
+        "topic": "Support", 
+        "awesome": "We always get great support & help when we ask for it!",
+        "crappy": "We keep getting stuck because we can’t get the support & help that we ask for."
     },
     {
         "sourceSet": sourceSet,
-        "area": "Pawns or players", 
-        "green": "We are in control of our destiny! We decide what to build and how to build it.",
-        "red": "We are just pawns in a game of chess, with no influence over what we build or how we build it."
+        "topic": "Pawns or players", 
+        "awesome": "We are in control of our destiny! We decide what to build and how to build it.",
+        "crappy": "We are just pawns in a game of chess, with no influence over what we build or how we build it."
     }
 ]);
 
@@ -76,14 +76,14 @@ collection.deleteMany({sourceSet: sourceSet});
 collection.insert([
     {
         "sourceSet": sourceSet,
-        "area": "Development Environment", 
-        "green": "Our development environment is a pleasure to use.",
-        "red": "Massive PITA."
+        "topic": "Development Environment", 
+        "awesome": "Our development environment is a pleasure to use.",
+        "crappy": "Massive PITA."
     },
     {
         "sourceSet": sourceSet,
-        "area": "Foosball",
-        "green": "Our foosball facilities are in top form.",
-        "red": "Broken men, square balls, and unlevel tables"
+        "topic": "Foosball",
+        "awesome": "Our foosball facilities are in top form.",
+        "crappy": "Broken men, square balls, and unlevel tables"
     }
 ]);
