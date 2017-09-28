87d427c10cf8799889383e7e024faa2b36eb4942
Jeff Ellis <jeff@ellises.us>
2017-09-28 16:43:30 -0400

    * Convert header to an AppBar * Add and Overview page * Remove dead code

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-import AppBody from './components/AppBody'; 
+import AppBody from './components/AppBody';
+import AppHeader from './components/AppHeader'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <AppHeader/>
         <AppBody/>
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
