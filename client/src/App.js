1d6e578b073c3b8771efb1374179e6db48eb0d79
Jeff Ellis <jeff@ellises.us>
2017-09-29 16:08:08 -0400

    cleaning up

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
 import AppBody from './components/AppBody';
 import AppHeader from './components/AppHeader'; 
 
@@ -10,10 +9,6 @@ class App extends Component {
       <div className="App">
         <AppHeader/>
         <AppBody/>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Yo!</h1>
-        </header> */}
       </div>
     );
   }
